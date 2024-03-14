4bbc8a6fd9fb3ecb1c6321162525c7a49c5e0451
rampKKJ <107202129+rampKKJ@users.noreply.github.com>
2024-03-14 07:51:46 -0300

    fhx

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelectorAll("botao"):
+for (let i = 0; i < botoes . length; i++) {
+   botoes [i].onclick = function () {
+     for (let = 0; j <botoes.length; j++) {
+        botoes [j].classList.remove("ativo");
+     }
+
+     botoes [i].classList.add("ativo");
+    }
+}
+
