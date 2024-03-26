39c1e912d69e4fcfb370fe4ac237fb43be0bc85b
rampKKJ <107202129+rampKKJ@users.noreply.github.com>
2024-03-26 12:04:18 -0300

    hhh

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const botoes = document.querySelectorAll("botao");
+const textos = document.querySelectorAll(".abra-conteudo");
 for (let i = 0; i < botoes . length; i++) {
    botoes [i].onclick = function () {
      for (let = 0; j <botoes.length; j++) {
         botoes [j].classList.remove("ativo");
+        textos [j].classList.remove("ativo");
      }
 
      botoes [i].classList.add("ativo");
+     textos[i].classList.add("ativio");
     }
 }
 
